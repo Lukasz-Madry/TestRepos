daf2fb13bfd92d3812b5096142f21a732e4ba3ed
Łukasz Mądry <lukamad427@student.polsl.pl>
2020-10-09 15:13:44 +0200

    Learning from scratch

---
+++ Kurrr/Consol1/Consol1/Consol1.cpp
@@ -0,0 +1,47 @@
+﻿// Consol1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+#include<iostream>
+
+using namespace std;
+
+float metry;
+
+float ile_cali(float m)
+{
+	return m * 39.37;
+}
+
+float ile_yard(float x);
+
+void ile_mil(float m)
+{
+	cout << "mile: " << m * 0.0006213 << endl;
+}
+
+int main()
+{
+	cout << "Podaj metry: ";
+	cin >> metry;
+
+	cout <<"cale: "<<ile_cali(metry)<<endl;
+	cout << "jardy: " <<ile_yard(metry) << endl;
+	ile_mil(metry);
+	
+	return 0;
+}
+
+float ile_yard(float x)
+{
+	return x * 1.0936;
+}
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
