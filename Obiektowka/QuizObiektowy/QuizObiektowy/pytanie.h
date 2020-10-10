0e3f55ad18acd8552bdab8aceb62c86c0f83c3ca
Łukasz Mądry <lukamad427@student.polsl.pl>
2020-10-10 19:24:54 +0200

    Quiz wykonany w obiektowce

---
+++ Obiektowka/QuizObiektowy/QuizObiektowy/pytanie.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Pytanie
+{
+public:
+	string tresc;
+	string a, b, c, d;
+	int nr_pytania;
+	string poprawna;
+	string odpowiedz;
+	int punkt;
+
+	void wczytaj(); //wczytaj z pliku
+	void zadaj(); //zadaje pytanie
+	void sprawdz();//sprawdza pytanie
+	
+};
